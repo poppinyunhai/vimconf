5f0455f5ca569bdfaff822e16e132bcd5c9bfb2c
daqing <daqing1986@gmail.com>
2010-02-11 13:41:42 +0800

    clean up after copying files

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #! /usr/bin/env bash
 cp ./vimrc ~/.vimrc
 mkdir ~/.vim
+
+# copy files
 for i in `ls`; do
     cp -R ./$i ~/.vim/
+done
+
+# clean up
+for f in deploy.sh README.md vimrc; do
+    rm ~/.vim/$f
 done
